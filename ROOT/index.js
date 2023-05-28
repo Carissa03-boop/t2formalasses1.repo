d91672737722a74b3383336900306b00a0d3ddfa
Carissa03-boop <125882806+Carissa03-boop@users.noreply.github.com>
2023-05-28 22:36:27 +0200

    Finished

--- ROOT/index.js
+++ ROOT/index.js
@@ -1,12 +1,13 @@
 addStudent = () => {
-    let first = document.getElementById("first").value;
-    let last = document.getElementById("last").value;
-    let age = +document.getElementById("age").value;
+    let nameSurname = document.getElementById("nameSurname").value;
+    let email = document.getElementById("email").value;
+    let contactNumber = +document.getElementById("contactNumber").value;
     let subject = document.getElementById("subject").value;
+    let defaultCheck1 = document.getElementById("defaultCheck1").value;
 
-    if(age >= 18){
-        alert("Student:  " + first + " " + last + " has been added to " + subject); 
+    if(document.getElementById(defaultCheck1).checked){
+        alert("Welcome " + nameSurname + " ! Thank you for your interest in " + subject + "! We will contact you via the email provided: " + email +", or alternatively on the phone number:" + contactNumber + "."); 
     }else{
-        alert("The student " + first + " " + last + " is to young to enroll!");
+        alert("Well, well, well... why don't you want to sign up for our newsletter... you dont seem to be that interested " + nameSurname + " ! If you want to sign up, you better CHECK that newsletter box!");
     }
 }
